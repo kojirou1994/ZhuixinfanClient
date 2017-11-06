9e9ee6e335e8117245d935a8b3292e6992063f16
Kojirou <Kojirouhtc@gmail.com>
2017-11-06 22:58:34 +0800

    try

--- Sources/ZhuixinfanClient/RSSFeed.swift
+++ Sources/ZhuixinfanClient/RSSFeed.swift
@@ -13,6 +13,7 @@ func generateXML(sources: [(String, String)]) -> String {
         return """
             <item>
             <title>\(source.0)</title>
+            <link>\(source.1)</link>
             <description>\(source.0)</description>
             <enclosure type="application/x-bittorrent" url="\(source.1)"/>
             </item>

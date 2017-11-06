0657db3e4aecc8d8aec12d50628688f3cf897dbe
Kojirou <Kojirouhtc@gmail.com>
2017-11-06 22:20:30 +0800

    fix

--- Sources/ZhuixinfanClient/RSSFeed.swift
+++ Sources/ZhuixinfanClient/RSSFeed.swift
@@ -27,11 +27,6 @@ func generateXML(sources: [(String, String)]) -> String {
     <title>Zhuixinfan</title>
     <link>http://www.zhuixinfan.com/main.php</link>
     <description>Free japan dramas.</description>
-    <item>
-    <title>RSS Tutorial</title>
-    <link>http://www.w3school.com.cn/rss</link>
-    <description>New RSS tutorial on W3School</description>
-    </item>
     \(sources.map(generateItem).joined(separator: "\n"))
     </channel>
 

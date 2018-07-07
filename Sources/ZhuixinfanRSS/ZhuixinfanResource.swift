d867ca11a12c01d2fa9ec5c6efe9be790384bdd6
Kojirou <Kojirouhtc@gmail.com>
2018-07-07 17:08:19 +0800

    clear

--- Sources/ZhuixinfanRSS/ZhuixinfanResource.swift
+++ Sources/ZhuixinfanRSS/ZhuixinfanResource.swift
@@ -24,26 +24,23 @@ struct ZhuixinfanResource: Codable {
         }
         self.sid = sid
         self.text = text
-        self.ed2k = ed2k.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        self.magnet = magnet.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        if ed2k.hasPrefix("magnet"), magnet.hasPrefix("ed2k") {
+            self.ed2k = magnet.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            self.magnet = ed2k.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        } else {
+            self.ed2k = ed2k.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            self.magnet = magnet.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        }
         self.drive1 = drive1
         self.drive2 = drive2
     }
     
-    var insertQuery: String {
-        return """
-        INSERT INTO viewresource
-        (sid, text, ed2k, magnet, drive1, drive2)
-        VALUES
-        (\(sid), "\(text)", "\(ed2k)", "\(magnet)", "\(drive1 ?? "")", "\(drive2 ?? "")");
-        """
-    }
 }
 
 extension ZhuixinfanResource: Model {
     
     static var tableName: String {
-        return "viewresource"
+        return "resource"
     }
     
     static var idColumnName: String {
